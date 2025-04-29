acccd7a2ad69781f26531bb7d703a3b6b3b21c38
Blueharmel <dldjdtjr@gmail.com>
2025-04-29 12:33:44 +0900

    youngseok, fix: 고객 목록에 삭제된 고객만 선택기능 추가  + 상세 페이지에서 입력값이 없는 필드에 선택 안함 표시 #101

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -1,18 +1,16 @@
 import type { PaginationDto } from './pagination';
-import type { ConsultationResDto } from './consultation';
 
 export interface Customer {
   id: number;
-  name: string;
+  name?: string;
   email?: string;
   contact: string;
   birthDate?: string; // 선택적
   gender?: 'M' | 'F'; // 선택적
   memo?: string; // 선택적
   ageGroup?: number;
-  consultations?: ConsultationResDto[]; // 선택적
-  createdAt: string;
-  updatedAt: string | null;
+  createdAt?: string;
+  updatedAt?: string | null;
   deletedAt?: string | null;
 }
 
@@ -29,14 +27,14 @@ export interface CreateCustomerReqDto {
 
 export interface CreateCustomerResDto {
   id: number;
-  name: string;
-  email: string;
+  name?: string;
+  email?: string;
   contact: string;
-  birthDate: string;
+  birthDate?: string;
   gender?: 'M' | 'F' | undefined;
   memo?: string;
-  createdAt: string;
-  updatedAt: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface CustomerListResDto {
