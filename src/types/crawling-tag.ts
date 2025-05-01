156c4b45448f58edfa012693ef38620419fb1728
kim <gimgoon54321@gmail.com>
2025-05-01 17:33:42 +0900

    hyunho, fix: SuccessResponse  인터페이스 api.ts 파일에 있는 것으로 변경

--- src/types/crawling-tag.ts
+++ src/types/crawling-tag.ts
@@ -3,10 +3,3 @@ export interface CrawlingTagResDto {
   type: string;
   value: string;
 }
-
-export interface SuccessResponse<T> {
-  success: boolean;
-  message: string;
-  code: string;
-  data: T;
-}
